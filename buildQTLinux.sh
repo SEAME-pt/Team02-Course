775e7b03e1491c985f6d71a839fc82376d290012
Rui Pires <ruipedro.pires999@gmail.com>
2025-01-07 11:04:38 +0000

    enhance Docker setup for cross-platform builds and add user permissions for Docker and KVM

--- buildQTLinux.sh
+++ buildQTLinux.sh
@@ -19,13 +19,33 @@ apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Add the current user to the docker group
+usermod -aG docker ${whoami}
 usermod -aG docker ${SUDO_USER}
 
 # Enable Docker service
 systemctl enable docker
 systemctl start docker
 
-# Build the Docker image
+apt install qemu-system-x86 qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+
+usermod -aG kvm $(whoami)
+usermod -aG kvm $(SUDO_USER)
+usermod -aG libvirt $(whoami)
+usermod -aG libvirt $(SUDO_USER)
+
+systemctl enable libvirtd
+systemctl start libvirtd
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+docker buildx create --use --name mybuilder
+docker buildx inspect mybuilder --bootstrap
+
+docker buildx build --platform linux/arm64 --load -f DockerFileRasp -t raspimage .
+
+docker create --name temp-arm raspimage
+docker cp temp-arm:/build/rasp.tar.gz ./rasp.tar.gz
+
 docker build -t qtcrossbuild .
 
 echo "Docker installation and image 
