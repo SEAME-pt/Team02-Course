13d31911d3063b50f8b54e795abfd21925b4b23a
Rui Pires <ruipedro.pires999@gmail.com>
2025-01-17 12:33:53 +0000

    chore: update submodules for InstrumentCluster and JetRacer

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Initialize submodules
 git submodule init
 
-# Synchronize submodules
 git submodule sync
 
-# Update submodules to the latest commit on the remote branch
 git submodule update --remote --merge
