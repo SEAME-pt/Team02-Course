67117a264f766b0a8bea8828af4f8ba051dd9da3
Rui Pires <ruipedro.pires999@gmail.com>
2025-01-17 14:22:38 +0000

    chore: simplify Dockerfile paths and update deployment scripts

--- deployToJetson.sh
+++ deployToJetson.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 # Check if the hostPath argument is provided
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-  echo "Usage: $0 <Repo Path> <Project Path> <Executable>"
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <Project dir> <Executable>"
   exit 1
 fi
 
-projectDir=$2
-repoPath=$1
-hostPath=$repoPath/$projectDir
+projectDir=$1
 
 # Check if the provided path exists
-if [ ! -d "$hostPath" ]; then
-  echo "Error: The provided path ($hostPath) does not exist."
+if [ ! -d "$projectDir" ]; then
+  echo "Error: The provided path ($projectDir) does not exist."
   exit 1
 fi
 
-executable=$3
+executable=$2
 piUserName=team02
 piIpAddress=10.21.221.71
 piPath=/home/team02
@@ -25,13 +23,12 @@ piPass=seameteam2
 echo "build docker image to build app"
 docker build -f DockerfileDeployJetson \
     --build-arg projectDir=/$projectDir \
-    --build-arg repoPath=$repoPath \
     -t final-app .
 echo "Remove tmpapp container if it is exist"
 docker rm -f tmpapp
 echo "Create a tmp container to copy binary"
 docker create --name tmpapp final-app
 echo "Copy the binary from tmp container"
-docker cp tmpapp:$hostPath/$executable ./$executable
+docker cp tmpapp:/home/$projectDir/$executable ./$executable
 echo "Send binary to rasp over scp"
 sshpass -p "$piPass" scp $executable "$piUserName"@"$piIpAddress":"$piPath"
